2caf09c33fb481b5b9d4fb4fe27047c379761b7f
ben890524 <ben10723021@gmail.com>
2021-11-15 12:49:04 +0800

    add jeiba

--- project_linebot/project_linebot/tokenizer_predictions.py
+++ project_linebot/project_linebot/tokenizer_predictions.py
@@ -1,4 +1,5 @@
 import json
+import jieba
 import numpy as np
 import tensorflow.keras as keras
 from keras.models import load_model
@@ -15,8 +16,15 @@ model_Bi_LSTM = load_model(cur_path + '/project_linebot/models/model_Bi_LSTM.h5'
 model_LSTM_ONLY = load_model(cur_path + '/project_linebot/models/model_LSTM_ONLY.h5')
 
 def token_padding(inputs):
+    inputs = jieba.cut(str(inputs))
+    text_tmp = []
+    for word in inputs:
+        if word == '!' or word == '《' or word == '》' or word == '\xa0' or word == ' ' or word == '\n' or word == '/n' or word == '(' or word == ')' or word == '『' or word == '』' or word == '...' or word == '（' or word == '）' or word == '：' or word == '?' or word == '？' or word == '！' or word == '\u3000' or word == '】' or word == '【' or word == '」' or word == '「' or word == '，' or word == '、' or word == '╱' or word == '。' or word == '；' or word == '·' or word == '..' or word == '．' or word == '.' or word == ',' or word == '～' or word == '“' or word == '”' or word == '/' or word == '㈠' or word == '㈡' or word == '／':  
+            word = ''
+        else:
+            text_tmp.append(word)
     trantotoken = []
-    for input in inputs:
+    for input in text_tmp:
         if input in tokenizer:
             trantotoken.append(tokenizer[input])
         else:
@@ -28,5 +36,5 @@ def predictions(trantotoken):
     prediction_GRU = np.argmax(model_GRU_ONLY.predict(trantotoken),axis=1)[0]
     prediction_BiLSTM = np.argmax(model_Bi_LSTM.predict(trantotoken),axis=1)[0]
     prediction_LSTM = np.argmax(model_LSTM_ONLY.predict(trantotoken),axis=1)[0]
-    # print(prediction_GRU, prediction_BiLSTM, prediction_LSTM)
+    print(prediction_GRU, prediction_BiLSTM, prediction_LSTM)
     return prediction_GRU, prediction_LSTM, prediction_BiLSTM
